7a050f4b32acd50e5220379dee9c35c4c1090e62
MahmoudHooda2019 <mh20192004@gmail.com>
2026-03-17 10:55:15 +0200

    switch to github pages

--- rena/frontend/vite.config.ts
+++ rena/frontend/vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/rena-platform/',
   plugins: [vue()],
   envDir: '../../', // Look for .env in the root
   resolve: {
